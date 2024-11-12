e187839752d099c69118a182645a0292995bb127
ac1dgit <nick.papadim@outlook.com>
2024-11-12 17:26:30 +0200

    test 2

--- arraytesting.py
+++ arraytesting.py
@@ -1,5 +1,16 @@
 import numpy as np
 
-A = np.zeros((5,5))
+size1 = 8
+size2 = 10
+
+A = np.zeros((size1,size2))
+
+print(A)
+count = 1
+
+for s1 in range(size1):
+    for s2 in range(size2):
+        A[s1,s2] = count
+        count += 1
 
 print(A)
