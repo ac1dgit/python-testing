9ea7d1153c9ac5c9a4f005d4bb441dcbfc508fec
ac1dgit <151084388+ac1dgit@users.noreply.github.com>
2024-11-14 22:05:32 +0200

    added single port test

--- onboard testing/ad test code/pot test.py
+++ onboard testing/ad test code/pot test.py
@@ -7,5 +7,5 @@ ADC = ADS1256.ADS1256()
 ADC.ADS1256_init()
 
 while(True):
-    ADC_Value = ADC.ADS1256_GetAll()
-    print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff))
+    ADC_Value = ADC.ADS1256_GetChannalValue(0)
+    print ("0 ADC = %lf"%(ADC_Value*5.0/0x7fffff))
