c549f0430ec7d4fb85893f5a41f7bdaeb02bb72e
ac1dgit <151084388+ac1dgit@users.noreply.github.com>
2024-11-18 17:19:04 +0200

    sample rate testing implementation

--- onboard testing/ad test code/sampleratetest.py
+++ onboard testing/ad test code/sampleratetest.py
@@ -1,11 +1,22 @@
 import time
+import numpy as np
 import ADS1256
 import RPi.GPIO as GPIO
 
-
 ADC = ADS1256.ADS1256()
 ADC.ADS1256_init()
-ADC.ADS1256_Read_data(1)
+ADC.ADS1256_ConfigADC(0,0x82)
 
-ADC_Value = ADC.ADS1256_GetChannalValue(0)
+samples = 100
 
+time_start = time.time_ns()
+for i in range(samples):
+    adc=ADC.ADS1256_GetChannalValue(0)
+    time_stop = time.time_ns()
+
+elapsed_time = time_stop-time_start
+
+time_s = elapsed_time/np.power(10,9)
+hz = samples/time_s
+print(time_s)
+print(hz)
