fb95e22703e62c05d4cc55898191316c02b1e36c
ac1dgit <151084388+ac1dgit@users.noreply.github.com>
2024-11-19 14:23:13 +0200

    added data logging and saving

--- onboard testing/ad test code/sampleratetest.py
+++ onboard testing/ad test code/sampleratetest.py
@@ -1,21 +1,25 @@
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 import ADS1256
 import RPi.GPIO as GPIO
 
 ADC = ADS1256.ADS1256()
 ADC.ADS1256_init()
-ADC.ADS1256_ConfigADC(0,0x82)
+ADC.ADS1256_ConfigADC(0,0xB2)
 
 samples = 100
+data = np.zeros((samples,2))
 
 time_start = time.time_ns()
 for i in range(samples):
-    adc=ADC.ADS1256_GetChannalValue(0)
-    time_stop = time.time_ns()
+    data[i,:] = [(time.time_ns()-time_start)/np.power(10,6),ADC.ADS1256_GetChannalValue(0)*5.0/0x7fffff]
+time_stop = time.time_ns()
+
+np.savetxt('testsave.txt', data)
+print(data)
 
 elapsed_time = time_stop-time_start
-
 time_s = elapsed_time/np.power(10,9)
 hz = samples/time_s
 print(time_s)
