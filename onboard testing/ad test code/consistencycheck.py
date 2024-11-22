0dc468244ffb381e9a81915569eb0315ad6930d4
ac1dgit <151084388+ac1dgit@users.noreply.github.com>
2024-11-22 14:37:33 +0200

    changed to mae

--- onboard testing/ad test code/consistencycheck.py
+++ onboard testing/ad test code/consistencycheck.py
@@ -1,17 +1,16 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 data = np.load('testsave.npy')
 timeseries = data[:,0]
 
-timeseries = np.delete(timeseries, range(1000))
+timeseries = np.delete(timeseries, range(2000))
 
 samplesize = np.size(timeseries)
 print(samplesize)
 
 elapsed_time = (timeseries[samplesize-1] - timeseries[0])/np.power(10,6)
-hz = samplesize/elapsed_time
-period = 1/hz
+hzperf = samplesize/elapsed_time
+period = 1/hzperf
 
 gapstrue = np.zeros((samplesize-1,1))
 gapsperf = np.zeros((samplesize-1,1))
@@ -21,4 +20,6 @@ for i in range(samplesize-1):
     gapstrue[i,0] = (timeseries[i+1] - timeseries[i])/np.power(10,6)
 
 print(gapstrue)
-print(np.square(np.subtract(gapstrue,gapsperf)).mean()) # MSE
+print("test sample count:", samplesize)
+print("test elapsed time (ms):", elapsed_time)
+print("sample time mean absolute error (ms):", np.sqrt(np.square(np.subtract(gapstrue,gapsperf)).mean()))
