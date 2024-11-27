1eb9148aefa4eb62ae419519e62213bf650823b8
ac1dgit <151084388+ac1dgit@users.noreply.github.com>
2024-11-27 18:40:01 +0200

    2 channels at 1khz

--- onboard testing/ad test code/sampleratetest.py
+++ onboard testing/ad test code/sampleratetest.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 
 ADC = ADS1256.ADS1256()
 ADC.ADS1256_init()
-ADC.ADS1256_ConfigADC(0,0xB0)
+ADC.ADS1256_ConfigADC(0,0xF0)
 #'ADS1256_30000SPS' : 0xF0
 #'ADS1256_15000SPS' : 0xE0
 #'ADS1256_7500SPS' : 0xD0
@@ -26,8 +26,8 @@ ADC.ADS1256_ConfigADC(0,0xB0)
 
 hztarget = 1000
 
-samples = 3000
-data = np.zeros((samples,2))
+samples = 300
+data = np.zeros((samples,3))
 
 t=time.time()
 period=1/hztarget
@@ -36,7 +36,7 @@ time_start = time.time_ns()
 
 for i in range(samples):
     t+=period
-    data[i,:] = [time.time_ns(),ADC.ADS1256_GetChannalValue(7)*5.0/0x7fffff]
+    data[i,:] = [time.time_ns(),ADC.ADS1256_GetChannalValue(0)*5.0/0x7fffff,ADC.ADS1256_GetChannalValue(1)*5.0/0x7fffff]
     time.sleep(max(0,t-time.time()))
 time_stop = time.time_ns()
 
