9d5064b0baa89ee2b937c89d6856598df3ee8d56
ac1dgit <151084388+ac1dgit@users.noreply.github.com>
2025-07-17 16:12:16 +0300

    test

--- MCC118 onboard testing/continuous_scan_to_csv.py
+++ MCC118 onboard testing/continuous_scan_to_csv.py
@@ -18,7 +18,7 @@ def main():
     num_channels = len(channels)
     samples_per_channel = 0
     options = OptionFlags.CONTINUOUS
-    scan_rate = 3000.0
+    scan_rate = 1000.0
     
     try:
         # Select an MCC 118 HAT device to use.
